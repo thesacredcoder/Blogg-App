11089b1729c0da0b5ec9c23634477258edd26370
thescaredhacker <56783866+thescaredhacker@users.noreply.github.com>
2020-06-24 11:38:46 +0530

    Add files via upload
    Rest of the files were the dependencies which I omitted.

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+//App config
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTful Routes
+app.get("/", (req, res) => {
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("ERROR");
+        } else {
+            res.render("index", {blogs: blogs})
+        }
+    })
+    res.render("index")
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is running");
+})
